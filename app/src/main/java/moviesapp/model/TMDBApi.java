c4938285879777170e71c388f6f59f89bc888876
a21202776 <khadija.ammari@etu.univ-amu.fr>
2024-01-29 11:49:21 +0100

    création de la classe TMDBApi et clé API

---
+++ app/src/main/java/moviesapp/model/TMDBApi.java
@@ -0,0 +1,9 @@
+package moviesapp.model;
+
+import java.io.BufferedReader;
+import java.net.HttpURLConnection;
+import java.net.URL;
+
+public class TMDBApi {
+    private static final String API_KEY = "c2e5eea5f9078e7bd27be9838d32abf8";
+}
