9ddd3e1be7dc7845863a011ec8c17a0aa352e4bc
f22021762 <mohamedfhafah975@gmail.com>
2024-01-29 15:42:11 +0100

    documentation de la classe favorites

--- app/src/main/java/moviesapp/model/TMDBApi.java
+++
@@ -1,9 +0,0 @@
-package moviesapp.model;
-
-import java.io.IOException;
-
-public class TMDBApi {
-    private static final String API_KEY = "VotreCleAPI"; // Remplacez par votre clé API
-    private static final String BASE_URL = "https://api.themoviedb.org/3/movie/";
-
-}
