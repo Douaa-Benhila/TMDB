f40948c76e3b288e31ef6a70b2e63a8463332b63
f22021762 <mohamedfhafah975@gmail.com>
2024-02-13 14:01:10 +0100

    Creation de la classe MovieTileController et le fishier fxml afin de controller l'affichage des films comme des blocks

---
+++ app/src/main/java/moviesapp/controller/MovieTileController.java
@@ -0,0 +1,24 @@
+package moviesapp.controller;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.image.ImageView;
+
+
+public class MovieTileController {
+    @FXML
+    private ImageView posterImage;
+    @FXML
+    private Label titleLabel;
+    @FXML
+    private Label yearLabel;
+    @FXML
+    private Label ratingLabel;
+    @FXML
+    private Button addToFavoritesButton;
+    @FXML
+    private ImageView starImageView;
+
+
+}
