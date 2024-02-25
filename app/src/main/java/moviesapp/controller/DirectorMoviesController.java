e20ee836c503392b9aee98791b3bf250ffa90f80
douaa <douaa.BENHILA@etu.univ-amu.fr>
2024-02-25 23:27:31 +0100

    modification apres probleme de merge

--- app/src/main/java/moviesapp/controller/DirectorMoviesController.java
+++ app/src/main/java/moviesapp/controller/DirectorMoviesController.java
@@ -1,40 +1,21 @@
 package moviesapp.controller;
 
-import javafx.application.Platform;
 import javafx.fxml.FXML;
-
-import javafx.scene.control.ListView;
-import moviesapp.ApiManager.TMDBApi;
-
-import java.io.IOException;
-import java.util.List;
+import javafx.scene.control.Label;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
+import moviesapp.model.Author;
 
 public class DirectorMoviesController {
 
-
-
     @FXML
-    private ListView<String> moviesListView;
+    private ImageView directorImageView;
+    @FXML private Label directorNameLabel;
+    @FXML private Label directorBiographyLabel;
 
-
-    private TMDBApi tmdbApi = new TMDBApi();
-
-    public void loadMovies(String directorId) {
-        new Thread(() -> {
-            try {
-                List<String> movieTitles = tmdbApi.getMoviesByDirector(directorId);
-                Platform.runLater(() -> {
-                    System.out.println("Affichage des films : " + movieTitles);
-                    moviesListView.getItems().setAll(movieTitles);
-                });
-            } catch (IOException | InterruptedException e) {
-                e.printStackTrace();
-                Platform.runLater(() -> {
-                    moviesListView.getItems().setAll("Impossible de charger les films");
-                });
-            }
-        }).start();
+    public void setDirectorDetails(Author author) {
+        directorNameLabel.setText(author.getName());
+        directorBiographyLabel.setText(author.getBiography());
+        directorImageView.setImage(new Image(author.getImageUrl()));
     }
-
-
 }
