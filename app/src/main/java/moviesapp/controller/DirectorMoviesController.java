2fb23564d7a3738890096338b86007e173257cbd
Ben Ismail Fatma <benismailfatma8@gmail.com>
2024-02-26 23:35:50 +0100

    modification des couleurs de l'interface graphique et la documentation de la classe AppController et la classe DirectorMoviesController

--- app/src/main/java/moviesapp/controller/DirectorMoviesController.java
+++ app/src/main/java/moviesapp/controller/DirectorMoviesController.java
@@ -7,7 +7,10 @@ import moviesapp.ApiManager.TMDBApi;
 import java.io.IOException;
 import java.util.List;
 
-
+/**
+ * Contrôleur pour gérer l'affichage des films d'un réalisateur spécifique.
+ * Utilise TMDBApi pour récupérer les films par identifiant de réalisateur et les affiche dans un ListView.
+ */
 public class DirectorMoviesController {
 
     @FXML
@@ -15,6 +18,10 @@ public class DirectorMoviesController {
 
     private TMDBApi tmdbApi = new TMDBApi();
 
+    /**
+     * Charge et affiche les films d'un réalisateur spécifique.
+     * @param directorId L'identifiant du réalisateur dont les films doivent être chargés.
+     */
     public void loadMovies(String directorId) {
         new Thread(() -> {
             try {
